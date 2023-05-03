6ac35ab0d138a10f4ca8c4547ea296c5dbfcb0ef
Jose <josemonagas212@gmail.com>
2023-05-02 22:10:00 -0400

    added h1 tag to display current player turn, fixed issue with board not rendering, probably a hidden symbol causing an error in the script

--- app.js
+++ app.js
@@ -20,15 +20,29 @@ init();
 
 function init() {
   turn = -1; // first player to start the game
+
+  // 2D array, 0 represents no mark at current position -- turn this -90 deg
   board = [
-    // 0 represents no mark at current position
-    [O, O, O], // col 0
-    [O, O, O], // col 1
-    [O, O, O], // col 2
+    [0, 0, 0], // col 0
+    [0, 0, 0], // col 1
+    [0, 0, 0], // col 2
     //  r0 r1 r2
   ];
   winner = null; // indicates that there's no winner yet and keeps the game going
   render();
 }
 
-function render() {}
+function render() {
+  // render() transfers the state of the app to the DOM
+  renderBoard();
+  renderTurn();
+  renderControls();
+}
+
+function renderBoard() {}
+
+function renderMessage() {}
+
+function renderControls() {}
+
+console.log(board);
